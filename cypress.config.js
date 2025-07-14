0df9e87a2d0e6f5321cccf2e48c4f131ecfdea3d
Burçak <sefere71@gmail.com>
2025-07-14 23:12:45 +0300

    add to pom model

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  
+  projectId: "zqj3ay",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
